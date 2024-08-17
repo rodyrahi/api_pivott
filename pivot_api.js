8ebb182e9e9e66c0ba8b7b343497e3c77395a806
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2024-08-17 11:18:43 +0530

    first commit

---
+++ pivot_api.js
@@ -0,0 +1,22 @@
+
+const express = require('express')
+const app = express()
+const port = 6060
+
+
+app.post('/api', (req, res) => {
+    const test = req.body;
+    console.log(test);
+    res.sendStatus(200);
+})
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World! , pivott here')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
