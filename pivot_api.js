90bb69e93a8d4343338992c8bb43e12722fbc43d
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2024-08-17 11:54:33 +0530

    oko

--- pivot_api.js
+++ pivot_api.js
@@ -1,24 +1,20 @@
+    const express = require('express')
+    const app = express()
+    const port = 6060
 
-const express = require('express')
-const app = express()
-const port = 6060
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
+    app.post('/api', (req, res) => {
+        const test = req.body;
+        console.log(test);
+        res.sendStatus(200);
+    })
 
-app.use(express.urlencoded({ extended: true }));
+    app.get('/', (req, res) => {
+      res.send('Hello World! , pivott here')
+    })
 
-app.post('/api', (req, res) => {
-    const test = req.body;
-    console.log(test);
-    res.sendStatus(200);
-})
-
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World! , pivott here')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+    })
